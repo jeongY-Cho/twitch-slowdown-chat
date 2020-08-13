d1bcf46274c0b7363118ac48b623ccba7dfbcd6b
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-08-12 21:18:27 -0400

    build

---
+++ build/precache-manifest.af5f9e1503676e375dc33eee95938b9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6cb7ab4ebc9abea6155ef0042fec126",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6474925bee2faac74a82",
+    "url": "/static/css/main.d21ed5f0.chunk.css"
+  },
+  {
+    "revision": "f86499ffaf65415d19b3",
+    "url": "/static/js/2.bdd9533f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bdd9533f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6474925bee2faac74a82",
+    "url": "/static/js/main.ab9bc500.chunk.js"
+  },
+  {
+    "revision": "c9f369373e65f251a4c2",
+    "url": "/static/js/runtime-main.e58abc5c.js"
+  }
+]);
